867f29c04be23c56323f917fabd934b3fc4ef083
Anatoliy Korovin <antkorwin@gmail.com>
2019-10-18 23:35:26 +1000

    initial commit

---
+++ src/com/antkorwin/unicoder/ConvertToUnicodeAction.java
@@ -0,0 +1,42 @@
+package com.antkorwin.unicoder;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import org.apache.commons.text.StringEscapeUtils;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created on 16/10/2019
+ * <p>
+ * TODO: replace on the JavaDoc
+ *
+ * @author Korovin Anatoliy
+ */
+public class ConvertToUnicodeAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent event) {
+
+        final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
+        final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
+        final Document document = editor.getDocument();
+
+        Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
+        int start = primaryCaret.getSelectionStart();
+        int end = primaryCaret.getSelectionEnd();
+
+        // Replace the selection with a unicode string.
+        String selectedText = document.getText(TextRange.from(start, end-start));
+        String result = StringEscapeUtils.escapeJava(selectedText);
+
+        WriteCommandAction.runWriteCommandAction(project,
+                                                 () -> document.replaceString(start, end, result));
+    }
+}
---
+++ src/icons/ActionIcons.java
@@ -0,0 +1,10 @@
+package icons;
+
+import javax.swing.*;
+
+import com.intellij.openapi.util.IconLoader;
+
+public class ActionIcons {
+
+	public static final Icon PLUGIN_ICON = IconLoader.getIcon("/icons/icon_16x16.svg");
+}
